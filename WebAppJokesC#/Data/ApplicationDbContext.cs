74d0a046854a3ca8c24176f3f312ff973123b286
Filip Ebert <filipebertcz@gmail.com>
2024-04-03 19:00:46 +0200

    Add project files.

---
+++ WebAppJokesC#/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAppJokesC_.Models;
+
+namespace WebAppJokesC_.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebAppJokesC_.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ WebAppJokesC#/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppJokesC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
